035f253ac106505dd970719aad77e749d11ea068
syl63 <samuelyliu2006@gmail.com>
2026-02-01 14:59:55 +0000

    Added Simple Program for Analogue Read

---
+++ simple_analgoue_read/simple_analgoue_read.ino
@@ -0,0 +1,15 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int raw = analogRead(34);
+  Serial.println(raw);
+  delay(100);
+}
