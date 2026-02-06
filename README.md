36e598820ff04bc28d87172b8ed4c87f61e919e1
yuyouyou032 <69706398+yuyouyou032@users.noreply.github.com>
2026-02-06 10:56:10 +0000

    adjusted motor torque, updated documentation

--- README.md
+++ README.md
@@ -1,5 +1,48 @@
+# Wiring
+
+# APPS
+
+5V input/output. Use a voltage divider/other approach to condition the analogue output to a range that can be read by ESP32
+
+# TPS
+
+
+# Nema 23 Stepper motor
+
+https://www.motechmotor.com/productDetail-0105-81.html
+
+step angle $1.8\degree$
+
+# TMC5160 driver board
+
+datasheet: [TMC5160 Datasheet (long))](https://www.analog.com/media/en/technical-documentation/data-sheets/TMC5160A_datasheet_rev1.18.pdf)
+
+Description: [TMC5160 BOB Description](https://www.analog.com/media/en/technical-documentation/data-sheets/tmc5160-bob_datasheet_rev1.10.pdf)
+
+Install this library developed by tommag. both `TMC5160.h` and `Bitfield.h` are included
+
+https://github.com/tommag/TMC5160_Arduino
+
+# Motor torque
+
+Adjust motor torque by writing to these registers (in setup, after SPI communication and motor has been initialised):
+
+```
+  motor.writeRegister(TMC5160_Reg::GLOBAL_SCALER, 200); //32-255
+  motor.writeRegister(TMC5160_Reg::IHOLD_IRUN, 0x00011F14);
+```
+
+The complete list of register can be found in /TMC5160_registers.h
+
+The `IHOLD_IRUN` register format:
+
+* Bits 0-4: ihold (in this case hex(14) = dec(20))(0-31) (set to <= 70% of irun)
+* Bits 8-12: irun (0-31)
+* Bits 16-19: iholddelay
+
+! For some reasons changing motorParams didn't work for me. had to write to register for the change to stick
+
 ## Resources
 
 - [PID Controller in C](https://deepbluembedded.com/esp32-timers-timer-interrupt-tutorial-arduino-ide/)
 - [ESP32 Arduino IDE Timer Interrupts Reference](https://deepbluembedded.com/esp32-timers-timer-interrupt-tutorial-arduino-ide/)
-
--- stepper_example/stepper_example.ino
+++ stepper_example/stepper_example.ino
@@ -21,9 +21,10 @@ Tie SPI_MODE to VCC_IO, SD_MODE to GND.
 #include <TMC5160_registers.h>
 
 #include <Arduino.h>
+#include <SPI.h>
 
 
-const uint8_t SPI_CS = 2; // CS pin in SPI mode
+const uint8_t SPI_CS = 5; // CS pin in SPI mode
 //const uint8_t SPI_DRV_ENN = 8;  // DRV_ENN pin in SPI mode
 
 TMC5160_SPI motor = TMC5160_SPI(SPI_CS); //Use default SPI peripheral and SPI settings.
@@ -40,21 +41,26 @@ void setup()
   // This sets the motor & driver parameters /!\ run the configWizard for your driver and motor for fine tuning !
   TMC5160::PowerStageParameters powerStageParams; // defaults.
   TMC5160::MotorParameters motorParams;
-  motorParams.globalScaler = 98; // Adapt to your driver and motor (check TMC5160 datasheet - "Selecting sense resistors")
-  motorParams.irun = 31;
-  motorParams.ihold = 16;
+
 
   SPI.begin();
   motor.begin(powerStageParams, motorParams, TMC5160::NORMAL_MOTOR_DIRECTION);
-
+  motorParams.globalScaler = 255; // Adapt to your driver and motor (check TMC5160 datasheet - "Selecting sense resistors")
+  motorParams.irun = 31;
+  motorParams.ihold = 20;
+  motor.writeRegister(TMC5160_Reg::GCONF, 0x00000004);
+  motor.writeRegister(TMC5160_Reg::GLOBAL_SCALER, 200);
+  motor.writeRegister(TMC5160_Reg::IHOLD_IRUN, 0x00011F14);
   // ramp definition
   motor.setRampMode(TMC5160::POSITIONING_MODE);
-  motor.setMaxSpeed(400);
-  motor.setAcceleration(500);
+  motor.setMaxSpeed(200);
+  motor.setAcceleration(300);
 
   Serial.println("starting up");
 
   delay(1000); // Standstill for automatic tuning
+      
+  Serial.println("setup complete ----------");
 }
 
 void loop()
@@ -64,13 +70,14 @@ void loop()
   static bool dir;
 
   // every n seconds or so...
-  if ( now - t_dirchange > 3000 )
+  if ( now - t_dirchange > 5000 )
   {
     t_dirchange = now;
 
     // reverse direction
     dir = !dir;
     motor.setTargetPosition(dir ? 200 : 0);  // 1 full rotation = 200s/rev
+    //switch between pos 200 and 0 depending on direction
   }
 
   // print out current position
@@ -85,5 +92,11 @@ void loop()
     Serial.print(xactual);
     Serial.print("\tcurrent speed : ");
     Serial.println(vactual);
+    Serial.print("GLOBAL_SCALER: ");
+    Serial.println(motor.readRegister(TMC5160_Reg::GLOBAL_SCALER));
+    Serial.print("IHOLD_IRUN: ");
+    Serial.println(motor.readRegister(TMC5160_Reg::IHOLD_IRUN), HEX);
+    Serial.print("GCONF: ");
+    Serial.println(motor.readRegister(TMC5160_Reg::GCONF), HEX);
   }
 }
